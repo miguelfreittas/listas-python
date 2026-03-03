4c1feb757aa0758d756e9292db92141179bcc6d0
miguel <miguelfreittas09@gmail.com>
2026-03-03 08:27:33 -0300

    Delete q5.py

--- q5.py
+++
@@ -1,7 +0,0 @@
-x = int(input("insira um numero "))
-if x % 4 == 0:
-    print("o numero e divisivel por 4")
-elif x % 5 == 0:
-    print("o numero e divisivel por 5")
-else:
-    print("o numero nao e divisivel por 4 nem por 5")
