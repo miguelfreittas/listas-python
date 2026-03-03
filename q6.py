49034940691c1d6562479d43bae54b40ef35c12d
miguel <miguelfreittas09@gmail.com>
2026-03-03 08:26:09 -0300

    Delete q6.py

--- q6.py
+++
@@ -1,9 +0,0 @@
-x = int(input("insira um numero de 4 digitos "))
-a = int(x % 10)
-b = int(x / 10) % 10
-c = int(x / 100) % 10
-d = int(x / 1000) % 10
-if a == b == c == d:
-    print("todos os digitos sao iguais ")
-else:
-    print("nao sao iguais")   
