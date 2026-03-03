6fcf74f5d634a74d99df719f29ab9bdd01d09e46
miguel <miguelfreittas09@gmail.com>
2026-03-03 08:26:00 -0300

    Delete q4.py

--- q4.py
+++
@@ -1,7 +0,0 @@
-x = int(input("insira o primeiro numero "))
-y = int(input("insira o segundo numero "))
-z = int(input("insira o terceiro numero "))
-if x == y == z:
-    print((x+y+z) * 3)
-else:
-    print(x+y+z)
